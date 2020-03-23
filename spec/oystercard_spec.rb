58a1ecb7ab9904b8342a746a2fa8541807b192b3
Harry Turnbull <hturnbull@live.co.uk>
2020-03-23 15:17:36 +0000

    added context block for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'responds to #balance' do
-    expect(subject).to respond_to :balance
-  end
-  it 'a newly created card has a balance of 0' do
-    expect(subject.balance).to be 0
+  context '#balance' do
+    it 'responds to #balance' do
+      expect(subject).to respond_to :balance
+    end
+    it 'a newly created card has a balance of 0' do
+      expect(subject.balance).to be 0
+    end
   end
 end
